91bb51a6fed899c4ce278e800460e7fde82e3c70
Dima <pelexov.mitya@gmail.com>
2023-06-30 16:55:45 +0300

    make DZ 15.1 fnd 15.2 but wiyh problem

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // -------ДЗ 11.1--------------
 
-//  let userNamber= prompt ('Введіть своє число і отримайте його в квадраті');
+// let userNamber = +prompt("Введіть своє число і отримайте його в квадраті");
+// let x = 2;
 
-//  alert (userNamber*userNamber);
+// alert(userNamber ** x);
 
 // -----------ДЗ 11.2-----------------
 
@@ -19,20 +20,17 @@
 
 // ---------------------ДЗ 11.3------------------------------
 
-// let userAnumber = prompt("Введіть число а");
-// let userBnumber = prompt("Введіть число b");
+// let userAnumber = +prompt("Введіть число а");
+// let userBnumber = +prompt("Введіть число b");
 
-// let sum = +userAnumber + +userBnumber;
+// let sum = userAnumber + userBnumber;
 // let difference = userAnumber - userBnumber;
 // let multiplication = userAnumber * userBnumber;
 // let division = userAnumber / userBnumber;
 
-// let sumText = "сумма чисел=";
-// let differenceText = ", різниця чисел=";
-// let multiplicationText = ", множення чисел=";
-// let divisionText = ", ділення чисел=";
-
-// alert(  sumText + sum +differenceText +difference +multiplicationText +multiplication +divisionText +division);
+// alert(
+//   `сумма чисел=${sum}, різниця чисел=${difference}, множення чисел=${multiplication}, ділення чисел=${division}`
+// );
 
 // ----------------------ДЗ 12.1---------------------------------
 
@@ -45,10 +43,13 @@
 //   alert("Ви  підходите");
 // }
 
+//  АНАЛОГІЧНИЙ ВАРІАНТ
+// userAge > 45 || userAge < 18 ? alert("Ви не підходите") : alert("Ви підходите");
+
 // --------------------ДЗ 12.2-----------------------------------
 
-// let userAnumber = prompt("Введіть число а");
-// let userBnumber = prompt("Введіть число b");
+// let userAnumber = +prompt("Введіть число а");
+// let userBnumber = +prompt("Введіть число b");
 // if (userAnumber <= 3) {
 //   alert("Не вірно");
 // } else if (userAnumber >= 12) {
@@ -60,6 +61,11 @@
 // } else {
 //   alert("Вірно");
 // }
+
+// АНАЛОГІЧНИЙ ВАРІАНТ
+// userAnumber <= 3 || userAnumber >= 12 || userBnumber >= 13 || userBnumber < 5
+//   ? alert("Не вірно")
+//   : alert(" Bірно");
 
 // -------------------ДЗ 12.3.-----------------------------------
 
@@ -96,6 +102,19 @@
 //   );
 // }
 
+// АНАЛОГІЧНИЙ ВАРІАНТ
+// userMonths < 6
+//   ? alert(
+//       `Шановний ${userName}, Ви внесли ${userSum} грн.під 15% річних, на термін ${userMonths} місяців. За цей період часу Ви заробите ${nBefore6} грн.`
+//     )
+//   : userMonths > 9
+//   ? alert(
+//       `Шановний ${userName}, Ви внесли ${userSum} грн.під 17% річних, на термін ${userMonths} місяців. За цей період часу Ви заробите ${nAfter9} грн.`
+//     )
+//   : alert(
+//       `Шановний ${userName}, Ви внесли ${userSum} грн.під 16% річних, на термін ${userMonths} місяців. За цей період часу Ви заробите ${nBetween6And9} грн.`
+//     );
+
 // -----------------------------------------ЗАДАЧА ПО ПІДТВЕРДЖЕННЮ ПАРОЛЯ ТА ЛОГІНА------------------------------------------------------------------------------------------------------------------
 
 // Якщо відвідувач вводить "Admin", тоді запропонуйте за допомогою prompt ввести пароль. Якщо введено порожній рядок або натиснуто Esc – показати “Скасовано”. Якщо введено інший рядок – тоді покажіть “Я вас не знаю”.
@@ -120,7 +139,135 @@
 // } else {
 //   alert("Я вас не знаю");
 // }
+
+// -------------------------------------------ЗАДАЧА ПО ВИЗНАЧЕННЮ ПОРИ РОКУ-------------------------
+
+// let userMonths = prompt("Введіть назву місяця").toLowerCase();
+
+// userMonths === "червень" || userMonths === "липень" || userMonths === "серпень"
+//   ? alert(`${userMonths} це літо`)
+//   : userMonths === "вересень" ||
+//     userMonths === "жовтень" ||
+//     userMonths === "листопад"
+//   ? alert(`${userMonths} це оcінь`)
+//   : userMonths === "грудень" ||
+//     userMonths === "січень" ||
+//     userMonths === "лютий"
+//   ? alert(`${userMonths} це зима`)
+//   : userMonths === "березень" ||
+//     userMonths === "квітень" ||
+//     userMonths === "травень"
+//   ? alert(`${userMonths} це весна`)
+//   : alert("такого місяця немає");
+
 // ------------------ДЗ 14.2---------------------------------------------------------
+
+// let a = +prompt("введіть а");
+// let b = +prompt("введіть b");
+
+// function max(a, b) {
+//   if (a > b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+// alert(max(a, b));
+
+// // ------------------ДЗ 14.3----------------------------------------------------------
+
+// let userAge = +prompt("введіть рік народження");
+// let yearNow = +"2023";
+
+// function age(yearNow, userAge) {
+//   return yearNow - userAge;
+// }
+
+// alert(age(yearNow, userAge));
+
+// // ------------------ДЗ 14.4----------------------------------------------------------
+
+// let clientYear = +prompt("введіть свій рік народження");
+// let userName = prompt("введіть своє ім'я");
+// let yearToday = +"2023";
+// let clientAge = yearToday - clientYear;
+// let yearsPensia = 60 - clientAge;
+
+// function yearsUntilRetirement(clientAge, userName) {
+//   if (clientAge >= 60) {
+//     console.log(`${userName} вже на пенсії`);
+//   } else {
+//     console.log(`${userName} вийде на пенсію через ${yearsPensia} років`);
+//   }
+// }
+
+// yearsUntilRetirement(clientAge, userName);
+
+// -------------------ПРАВИЛЬНИЙ ВАРІАНТ ВИРІШЕННЯ ПОПЕРЕДНІХ ДВУЧ ЗАДАЧ-------------------------------------
+
+// function calcAge(year) {
+//   return 2023 - year;
+// }
+
+// console.log(calcAge(1993)); -----> НА ЦЬОМУ ЗАКІНЧУЄТЬСЯ ДЗ 14.3
+
+// function yearsUntilRetirement(year, userName) {
+//   let age = calcAge(year);
+//   let retirement = 60 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${userName} вийде на пенсію через ${retirement} років`);
+//   } else {
+//     console.log(`${userName} вже на пенсії`);
+//   }
+// }
+
+// yearsUntilRetirement(1993, "Jack");
+
+// АБО
+
+// yearsUntilRetirement(+prompt("Ваш рік народження"), prompt("Ваше ім'я"));
+
+// ------------------------------------ДЗ 15.1--------------------------------------------
+function weekFn(n) {
+  switch (n) {
+    case 1:
+      alert("Понеділок");
+      break;
+
+    case 2:
+      alert("Вівторок");
+      break;
+
+    case 3:
+      alert("Середа");
+      break;
+
+    case 4:
+      alert("Четвер");
+      break;
+
+    case 5:
+      alert("П'ятниця");
+      break;
+
+    case 6:
+      alert("Субота");
+      break;
+
+    case 7:
+      alert("Неділя");
+      break;
+
+    default:
+      alert(null);
+  }
+}
+
+console.log(weekFn(+prompt("введіть номер дня тижня")));
+
+// -----------------------------------------ДЗ 15.2--------------------------------
 
 let a = +prompt("введіть а");
 let b = +prompt("введіть b");
@@ -133,33 +280,21 @@ function max(a, b) {
   }
 }
 
-alert(max(a, b));
-
-// ------------------ДЗ 14.3----------------------------------------------------------
-
-let userAge = +prompt("введіть рік народження");
-let yearNow = +"2023";
-
-function age(yearNow, userAge) {
-  return yearNow - userAge;
-}
-
-alert(age(yearNow, userAge));
-
-// ------------------ДЗ 14.4----------------------------------------------------------
-
-let clientYear = +prompt("введіть свій рік народження");
-let userName = prompt("введіть своє ім'я");
-let yearToday = +"2023";
-let clientAge = yearToday - clientYear;
-let yearsPensia = 60 - clientAge;
-
-function yearsUntilRetirement(clientAge, userName) {
-  if (clientAge >= 60) {
-    console.log(`${userName} вже на пенсії`);
+function min(a, b) {
+  if (a < b) {
+    return a;
   } else {
-    console.log(`${userName} вийде на пенсію через ${yearsPensia} років`);
+    return b;
   }
 }
 
-yearsUntilRetirement(clientAge, userName);
+let c = confirm("Сказати яке більше число?");
+function foo(c) {
+  if (c === true) {
+    alert(max(a, b));
+  } else {
+    alert(min(a, b));
+  }
+}
+
+console.log(foo(c));
